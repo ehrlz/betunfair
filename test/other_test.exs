51b71704679c4833498c2619882eb71bcca9a3c3
Daniel Milbradt <d.milbradt@alumnos.upm.es>
2023-05-30 23:11:02 +0200

    test user_create_deposit_withdraw_get

--- test/other_test.exs
+++ test/other_test.exs
@@ -1,17 +1,20 @@
 defmodule OtherTests do
   use ExUnit.Case
 
-  test "user_create_deposit_get" do
+  test "user_create_deposit_withdraw_get" do
     assert {:ok, _} = Betunfair.clean("testdb")
     assert {:ok, _} = Betunfair.start_link("testdb")
     assert {:ok, u1} = Betunfair.user_create("u1", "Daniel Milbradt")
     assert is_error(Betunfair.user_create("u1", "Daniel Milbradt"))
     assert is_ok(Betunfair.user_deposit(u1, 3500))
     assert is_ok(Betunfair.user_deposit(u1, 500))
+    assert is_ok(Betunfair.user_withdraw(u1, 1000))
+    assert is_error(Betunfair.user_withdraw(u1, -1))
     assert is_error(Betunfair.user_deposit(u1, -1))
     assert is_error(Betunfair.user_deposit(u1, 0))
     assert is_error(Betunfair.user_deposit("u11", 0))
-    assert {:ok, %{balance: 4000}} = Betunfair.user_get(u1)
+    assert is_error(Betunfair.user_withdraw(u1, 5000))
+    assert {:ok, %{balance: 3000}} = Betunfair.user_get(u1)
   end
 
   defp is_error(:error),do: true
