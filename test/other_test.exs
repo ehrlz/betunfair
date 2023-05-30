d3c6d1f13eac86074ecf79fdd2f3d0afa53dd934
Daniel Milbradt <d.milbradt@alumnos.upm.es>
2023-05-30 22:46:49 +0200

    Private functions to check error or success

---
+++ test/other_test.exs
@@ -0,0 +1,11 @@
+defmodule OtherTests do
+
+  defp is_error(:error),do: true
+  defp is_error({:error,_}), do: true
+  defp is_error(_), do: false
+
+  defp is_ok(:ok), do: true
+  defp is_ok({:ok,_}), do: true
+  defp is_ok(_), do: false
+
+end
